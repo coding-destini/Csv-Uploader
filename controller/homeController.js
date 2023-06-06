addfdf7365851f811be4f26bf4997b8bfe8ee845
coding-destini <akashshahngu@gmail.com>
2023-06-06 13:28:28 +0530

    Initial Commit

---
+++ controller/homeController.js
@@ -0,0 +1,11 @@
+const fileController = require('../controller/fileController')
+
+const uploadedFileNames=fileController.uploadedFileNames;
+const array = uploadedFileNames(); //This is the array containing the csv files
+
+module.exports.Files = (req,res)=>{
+    return res.render('show_all_files',{
+        files:array
+    })
+}
+
